9f8d7d34e9acc988c03d7ce368c35a1b0f88d57f
ASuhail8 <Abdulla.suhail8@gmail.com>
2018-03-28 19:06:40 +0530

    final maven

---
+++ src/com/qa/maven/maven.java
@@ -0,0 +1,14 @@
+package com.qa.maven;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class maven {
+	@Test
+	public void test()
+		{
+		Reporter.log("this is maven project",true);
+	}
+
+}
