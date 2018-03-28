8837d681dc0f98b3aa9f2aeb3c4bedcbfea4dcc7
ASuhail8 <Abdulla.suhail8@gmail.com>
2018-03-28 19:31:38 +0530

    yes

--- src/com/qa/maven/maven.java
+++ src/com/qa/maven/maven.java
@@ -2,13 +2,12 @@ package com.qa.maven;
 
 import org.testng.annotations.Test;
 import org.testng.Reporter;
-import org.testng.annotations.Test;
 
 public class maven {
 	@Test
 	public void test()
 		{
-		Reporter.log("this is maven project",true);
+		Reporter.log("this is maven",true);
 	}
 
 }
